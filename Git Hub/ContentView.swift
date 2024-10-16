68894ec836edd95c0135bba4c6979aa927079587
dmyers1048 <dmyers1@bsd220.org>
2024-10-16 09:43:06 -0500

    Initial Commit

---
+++ Git Hub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Git Hub
+//
+//  Created by Devan Myers on 10/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Git Hub/Git_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_HubApp.swift
+//  Git Hub
+//
+//  Created by Devan Myers on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Git_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
